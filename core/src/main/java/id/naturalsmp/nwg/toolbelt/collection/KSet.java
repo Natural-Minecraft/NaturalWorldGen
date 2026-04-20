63393c45f74d6b409367d037af53cf5b46128dda
AnakTentara <anaktentara25@gmail.com>
2026-04-20 21:31:30 +0700

    Fix: KSet with array constructor and static merge for BinaryOperator usage

--- core/src/main/java/id/naturalsmp/nwg/toolbelt/collection/KSet.java
+++ core/src/main/java/id/naturalsmp/nwg/toolbelt/collection/KSet.java
@@ -1,7 +1,9 @@
 package id.naturalsmp.nwg.toolbelt.collection;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class KSet<T> extends LinkedHashSet<T> {
     private static final long serialVersionUID = 1L;
@@ -12,12 +14,24 @@ public class KSet<T> extends LinkedHashSet<T> {
 
     public KSet(Collection<? extends T> c) { super(c); }
 
+    @SafeVarargs
+    public KSet(T... elements) {
+        super(elements.length);
+        addAll(Arrays.asList(elements));
+    }
+
     public KList<T> toKList() {
         return new KList<>(this);
     }
 
-    public KSet<T> merge(KSet<T> other) {
+    public KSet<T> merge(Set<T> other) {
         addAll(other);
         return this;
+    }
+
+    public static <T> KSet<T> merge(Set<T> a, Set<T> b) {
+        KSet<T> result = new KSet<>(a);
+        result.addAll(b);
+        return result;
     }
 }
